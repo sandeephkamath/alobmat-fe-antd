8a2bab3128c6d3f4a1bb198ffa5b47b7a36111c3
Sandeep Kamath <XPlode123>
2020-04-19 15:14:42 +0530

    Allow http

--- app.js
+++ app.js
@@ -5,5 +5,12 @@ let app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || '8080';
 app.set('port', port);
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
